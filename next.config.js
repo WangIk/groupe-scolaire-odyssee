8e17fde97b41a70e5f42d110d4bf2f2d2a9bcf5a
rrrecf <evelinbrid@gmail.com>
2025-01-26 21:54:19 +0100

    index

--- next.config.js
+++ next.config.js
@@ -12,31 +12,56 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  async redirects() {
+    return [
+      // Redirection des anciennes URLs
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/produit.html',
+        destination: '/produits',
+        permanent: true,
+      },
+      {
+        source: '/services',
+        destination: '/produits',
+        permanent: true,
+      },
+      {
+        source: '/cgi-sys/suspendedpage.cgi',
+        destination: '/',
+        permanent: true,
+      },
+      // Nettoyage des URLs avec #!
+      {
+        source: '/#!',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/produit.html#!',
+        destination: '/produits',
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
         source: '/:path*',
         headers: [
           {
+            key: 'X-Robots-Tag',
+            value: 'index, follow'
+          },
+          {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains'
           },
         ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.jackindustries.ma',
-          },
-        ],
-        destination: 'https://jackindustries.ma/:path*',
-        permanent: true,
       },
     ];
   },
